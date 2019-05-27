8e0673c9922fbd651117d7cb3072ac6f1a36be80
Simon Kunz <simon@tylinar.de>
2019-05-27 16:26:28 +0200

    WIP

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import lodash from 'lodash';
 const MAX_WIDTH_RESOLUTION = 4096;
 const MAX_HEIGHT_RESOLUTION = 2160;
 
+let buffer;
 let isPaused = false;
 let canvasDrawingContext;
 let wbarApi;
@@ -11,11 +12,9 @@ let options;
 
 function wrapWbarCApi() {
   return {
-    scan_image: wbarCApi.cwrap('scan_image', '', ['number, number, number']),
-    init: wbarCApi.cwrap('init', '', []),
-    clean_up: wbarCApi.cwrap('clean_up', '', ['number']),
-    create_buffer: wbarCApi.cwrap('create_buffer', 'number', ['number', 'number']),
-    destroy_buffer: wbarCApi.cwrap('destroy_buffer', '', ['number']),
+    scan_image: wbarCApi.cwrap('scan_image', 'number', ['number, number, number']),
+    init: wbarCApi.cwrap('init', 'number', ['number', 'number']),
+    clean_up: wbarCApi.cwrap('clean_up', '', ['number'])
   };
 }
 
@@ -34,11 +33,12 @@ function detectSymbols() {
     options.scanner_height,
   );
   const image = canvasDrawingContext.getImageData(0, 0, options.scanner_width, options.scanner_height)
-  const buffer = wbarApi.create_buffer(image.width, image.height);
   wbarCApi.HEAP8.set(image.data, buffer);
 
   var r = wbarApi.scan_image(buffer, image.width, image.height) //in here we might have leak
-  wbarApi.destroy_buffer(buffer);
+  console.log(r);
+
+  //wbarApi.destroy_buffer(buffer);
   var stop = (new Date).getTime();
 
   //console.log(stop - start);
@@ -71,7 +71,8 @@ export default {
     lodash.defaults(options, {'scanner_offset_x':0,
                               'scanner_offset_y': 0});
     //here get width and height from tracks
-    wbarApi.init();
+
+    buffer = wbarApi.init(options.width, options.height);
     setTimeout(detectSymbols, 0);
 
     wbarCApi['onDetected'] = callback;
--- wbar.c
+++ wbar.c
@@ -7,10 +7,26 @@
 extern void on_detected_callback(const char *detected_code);
 
 zbar_image_scanner_t *scanner = NULL;
+zbar_image_t *image = NULL;
+//int width = 0;
+//int height = 0;
 
 EMSCRIPTEN_KEEPALIVE
 int scan_image(uint8_t *raw, int width, int height)
 {
+    scanner = zbar_image_scanner_create();
+    zbar_image_scanner_set_config(scanner, 0, ZBAR_CFG_X_DENSITY, 0);
+    zbar_image_scanner_set_config(scanner, 0, ZBAR_CFG_Y_DENSITY, 1);
+
+    zbar_image_scanner_set_config(scanner, 0, ZBAR_CFG_ENABLE, 0);
+    zbar_image_scanner_set_config(scanner, ZBAR_EAN8, ZBAR_CFG_ENABLE, 1);
+    zbar_image_scanner_set_config(scanner, ZBAR_EAN13, ZBAR_CFG_ENABLE, 1);
+    zbar_image_scanner_set_config(scanner, ZBAR_UPCA, ZBAR_CFG_ENABLE, 1);
+    zbar_image_scanner_set_config(scanner, ZBAR_UPCE, ZBAR_CFG_ENABLE, 1);
+
+    image = zbar_image_create();
+    zbar_image_set_format(image, zbar_fourcc('Y', '8', '0', '0'));
+    zbar_image_set_size(image, width, height);
     //Grayscale
     uint8_t* gray_img = (uint8_t*)malloc(width * height * sizeof(uint8_t));
     for (int i = 0; i < width; ++i) {
@@ -21,56 +37,36 @@ int scan_image(uint8_t *raw, int width, int height)
         }
     }
 
-    zbar_image_t *image = zbar_image_create();
-
-    zbar_image_set_format(image, zbar_fourcc('Y', '8', '0', '0'));
-    zbar_image_set_size(image, width, height);
     zbar_image_set_data(image, gray_img, width * height, zbar_image_free_data);
-
-    int return_code = zbar_scan_image(scanner, image);
+    int return_code = zbar_scan_image(scanner, image); //
     if(return_code < 0) {
         return(-1);
     }
-
     const zbar_symbol_t *symbol = zbar_image_first_symbol(image);
     for (; symbol; symbol = zbar_symbol_next(symbol))
     {
         const char *detected_code = zbar_symbol_get_data(symbol);
         on_detected_callback(detected_code);
     }
-
-    free(gray_img);
+    //
+    //;
+    zbar_image_free_data(image);
+    //free(gray_img); //
     zbar_image_destroy(image);
+    free(image);
+    zbar_image_scanner_destroy(scanner);
     return (return_code);
 }
 
 EMSCRIPTEN_KEEPALIVE
-void init() {
-    scanner = zbar_image_scanner_create();
-    zbar_image_scanner_set_config(scanner, 0, ZBAR_CFG_X_DENSITY, 0);
-    zbar_image_scanner_set_config(scanner, 0, ZBAR_CFG_Y_DENSITY, 1);
-
-    zbar_image_scanner_set_config(scanner, 0, ZBAR_CFG_ENABLE, 0);
-    zbar_image_scanner_set_config(scanner, ZBAR_EAN8, ZBAR_CFG_ENABLE, 1);
-    zbar_image_scanner_set_config(scanner, ZBAR_EAN13, ZBAR_CFG_ENABLE, 1);
-    zbar_image_scanner_set_config(scanner, ZBAR_UPCA, ZBAR_CFG_ENABLE, 1);
-    zbar_image_scanner_set_config(scanner, ZBAR_UPCE, ZBAR_CFG_ENABLE, 1);
+uint8_t *init(int width, int height) {
+    //width = _width;
+    //height = _height;
+    return malloc(width * height * 4 * sizeof(uint8_t));
 }
 
 EMSCRIPTEN_KEEPALIVE
 void clean_up(uint8_t *p)
 {
     zbar_image_scanner_destroy(scanner);
-}
-
-EMSCRIPTEN_KEEPALIVE
-uint8_t *create_buffer(int width, int height)
-{
-    return malloc(width * height * 4 * sizeof(uint8_t));
-}
-
-EMSCRIPTEN_KEEPALIVE
-void destroy_buffer(uint8_t *p)
-{
-    free(p);
 }
